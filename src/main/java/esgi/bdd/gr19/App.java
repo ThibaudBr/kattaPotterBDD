c94bd38cf9e678e44553e1b6bdd573206bb92235
Briand <briandthibaud99@gmail.com>
2022-01-22 16:33:45 +0100

    First commit

---
+++ src/main/java/esgi/bdd/gr19/App.java
@@ -0,0 +1,13 @@
+package esgi.bdd.gr19;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/esgi/bdd/gr19/AppTest.java
@@ -0,0 +1,20 @@
+package esgi.bdd.gr19;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
