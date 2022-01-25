91b7b473b3c9288e3d27c0ab1369584b9adfa167
Briand <briandthibaud99@gmail.com>
2022-01-25 21:39:30 +0100

    add AppTest.java

--- src/test/java/esgi/bdd/gr19/AppTest.java
+++ src/test/java/esgi/bdd/gr19/AppTest.java
@@ -1,20 +1,89 @@
 package esgi.bdd.gr19;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+
+    List<Book> bookList = new ArrayList<>();
+    Book book1 = new Book(1);
+    Book book2 = new Book(2);
+    Book book3 = new Book(3);
+    Book book4 = new Book(4);
+    Book book5 = new Book(5);
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testBook1() {
+        bookList.add(book1);
+        assert PriceService.getPrice(bookList) == 8;
     }
+
+    @Test
+    public void testBook2() {
+        bookList.add(book1);
+        bookList.add(book1);
+        assert PriceService.getPrice(bookList) == 16 ;
+    }
+
+    @Test
+    public void testBook3() {
+        bookList.add(book1);
+        bookList.add(book2);
+        assert PriceService.getPrice(bookList) == (8 * 2) * 0.95;
+    }
+
+    @Test
+    public void testBook4() {
+        bookList.add(book1);
+        bookList.add(book1);
+        bookList.add(book2);
+        assert PriceService.getPrice(bookList) == (8 * 2) * 0.95 + 8;
+    }
+
+    @Test
+    public void testBook5() {
+        bookList.add(book1);
+        bookList.add(book1);
+        bookList.add(book2);
+        bookList.add(book3);
+        assert PriceService.getPrice(bookList) == (8 * 3) * 0.90 + 8;
+    }
+
+    @Test
+    public void testBook6() {
+        bookList.add(book1);
+        bookList.add(book1);
+        bookList.add(book2);
+        bookList.add(book3);
+        bookList.add(book4);
+        assert PriceService.getPrice(bookList) == (8 * 4) * 0.80 + 8;
+    }
+
+    @Test
+    public void testBook7() {
+        bookList.add(book1);
+        bookList.add(book1);
+        bookList.add(book2);
+        bookList.add(book3);
+        bookList.add(book4);
+        bookList.add(book5);
+        assert PriceService.getPrice(bookList) == (8 * 5) * 0.75 + 8;
+    }
+
+    @Test
+    public void testBook8() {
+        bookList.add(book1);
+        bookList.add(book1);
+        bookList.add(book2);
+        bookList.add(book2);
+        bookList.add(book3);
+        bookList.add(book3);
+        bookList.add(book4);
+        bookList.add(book5);
+        assert PriceService.getPrice(bookList) == (8 * 5) * 0.75 + (8*3) * 0.90;
+    }
+
 }
